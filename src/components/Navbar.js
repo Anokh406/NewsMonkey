8a09e1c62498fe878bad952a1a9ed3e051b0b349
Anokh406 <95044585+Anokh406@users.noreply.github.com>
2022-09-03 23:27:26 +0530

    adding button on page for next and past output see

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,19 @@ export class Navbar extends Component {
   render() {
     return (
      <>
-     <nav className="navbar navbar-expand-lg bg-light">
+     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">NewsMonkey</a>
+    <a className="navbar-brand text-light" href="/">NewsMonkey</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/home">Home</a>
+          <a className="nav-link active text-light" aria-current="page" href="/home">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">About</a>
+          <a className="nav-link text-light" href="/about">About</a>
         </li>
       </ul>
     </div>
